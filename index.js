f810c23d5ecb1f8712e48157916b98ea1469711e
NicolasM <nicolaschompi@gmail.com>
2024-12-10 07:54:57 -0500

    Avemaria que vuelta

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(require("./src/modulos/citizen.js"));
+app.use(require("./src/modulos/categoria.js"));
+app.use(require("./src/modulos/grado.js"));
+app.use(require("./src/modulos/roles.js"));
+app.use(require("./src/modulos/usuarios.js"));
+app.use(require("./src/modulos/delito.js"));
+
+const port = process.env.PORT || 4102;
+
+app.listen(port, () => {
+  console.log(`server runnig in : ${port}`);
+});
